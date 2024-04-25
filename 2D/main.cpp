63c72532058ac4dbbe59d20a29dd8fba9e27ffe0
Karlo Piskor <kpiskor@student.ethz.ch>
2024-04-25 17:46:10 +0200

    time for dinner

--- 2D/main.cpp
+++ 2D/main.cpp
@@ -2,17 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include <cmath>
+#include <exception>
+
+#define PRINT 1
+
+#define debug_printf(fmt, ...) \
+  do {                         \
+    if (PRINT) {               \
+      fprintf(stdout, fmt, __VA_ARGS__); \
+    }                          \
+  } while (0)
+
+#define debug_print(fmt) \
+  do {                   \
+    if (PRINT) {         \
+      fprintf(stdout, fmt); \
+    }                    \
+  } while (0)
+
 
 using namespace std;
 
 const int Nx = 400;    // resolution in x
-const int Ny = 400;    // resolution in y
+const int Ny = 100;    // resolution in y
 const int rho0 = 100;  // average density
 const int tau = 0.6;   // collision timescale
 const int Nt = 4000;   // number of timesteps
 
-void meshgrid(int x_coords[][Ny], int y_coords[][Ny]) {
+// Lattice speeds / weights
+const int NL = 9;
+const int idx[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+const int cxs[9] = {0, 0, 1, 1, 1, 0, -1, -1, -1};
+const int cys[9] = {0, 1, 1, 0, -1, -1, -1, 0, 1};
+const double w[9] = {4 / 9, 1 / 9,  1 / 36, 1 / 9, 1 / 36,
+                     1 / 9, 1 / 36, 1 / 9,  1 / 36};  // sums to 1
+
+void meshgrid(int **x_coords, int **y_coords) {
   // Fill coordinate matrices
   for (int i = 0; i < Ny; ++i) {
     for (int j = 0; j < Nx; ++j) {
@@ -22,7 +49,7 @@ void meshgrid(int x_coords[][Ny], int y_coords[][Ny]) {
   }
 }
 
-void roll_array(int i, int cx, int cy, double F[][Ny][Nx]) {
+void roll_array(int i, int cx, int cy, double ***F) {
   // Temporary array to hold rolled values
   double temp[Ny][Nx];
 
@@ -41,36 +68,68 @@ void roll_array(int i, int cx, int cy, double F[][Ny][Nx]) {
   }
 }
 
+double*** malloc_3d(int x, int y, int z) {
+  double ***array = (double***)malloc(x * sizeof(double**));
+  for (int i = 0; i < x; ++i) {
+    array[i] = (double**)malloc(y * sizeof(double*));
+    for (int j = 0; j < y; ++j) {
+      array[i][j] = (double*)malloc(z * sizeof(double));
+    }
+  }
+  return array;
+}
+
+
+int** malloc_2d(int x, int y) {
+  int **array = (int**)malloc(x * sizeof(int*));
+  for (int i = 0; i < x; ++i) {
+    array[i] = (int*)malloc(y * sizeof(int));
+  }
+  return array;
+}
+
+
+double** malloc_2d_double(int x, int y) {
+  double **array = (double**)malloc(x * sizeof(double*));
+  for (int i = 0; i < x; ++i) {
+    array[i] = (double*)malloc(y * sizeof(double));
+  }
+  return array;
+}
+
 
 // TODO CHECK TYPES IF ALL CORRECT
-
+// TODO arrays need to be initialized likely with 0 values
 int main() {
+
   // Lattice Boltzmann Simulation in 2D
+  debug_print("Starting\n");
 
-  // Lattice speeds / weights
-  int NL = 9;
-  int idx[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-  int cxs[9] = {0, 0, 1, 1, 1, 0, -1, -1, -1};
-  int cys[9] = {0, 1, 1, 0, -1, -1, -1, 0, 1};
-  double w[9] = {4 / 9, 1 / 9,  1 / 36, 1 / 9, 1 / 36,
-                 1 / 9, 1 / 36, 1 / 9,  1 / 36};  // sums to 1
-
+  
   // Initial conditions
-  double F[Nx][Ny][NL];  // distribution function
+  // distribution function
+ 
+  // TODO creating array directly segfaults because of size
+  double ***F = malloc_3d(Nx, Ny, NL);
+  debug_print("Initializing\n");
 
-  srand(42); // some seed
+  //srand(42);  // some seed
+
+  debug_print("Seed set\n");
 
   // Initialize F
   for (int i = 0; i < Nx; i++) {
     for (int j = 0; j < Ny; j++) {
       for (int k = 0; k < NL; k++) {
-        F[i][j][k] = 1 + 0.01 * (rand() % 100);  // TODO right?
+        F[i][j][k] = 1 + 0.01 * 0.5;  // TODO randomize
       }
     }
   }
 
-  int x_coords[Nx][Ny];
-  int y_coords[Nx][Ny];
+  // TODO maybe also malloc at some point
+  int **x_coords = malloc_2d(Nx, Ny);
+  int **y_coords = malloc_2d(Nx, Ny);
+  
 
   meshgrid(x_coords, y_coords);
 
@@ -101,10 +160,21 @@ int main() {
   }
 
   // Cylinder boundary
-  int cylinderX[Nx][Ny];
-  int cylinderY[Nx][Ny];
+  int **cylinderX = malloc_2d(Nx, Ny);
+  int **cylinderY = malloc_2d(Nx, Ny);
+  int **cylinder = malloc_2d(Nx, Ny);
 
   meshgrid(cylinderX, cylinderY);
+
+  // cylinder = (X - Nx/4)**2 + (Y - Ny/2)**2 < (Ny/4)**2
+  for (int i = 0; i < Nx; i++) {
+    for (int j = 0; j < Ny; j++) {
+      if ((pow(x_coords[i][j] - Nx / 4, 2) + pow(y_coords[i][j] - Ny / 2, 2)) < pow(Ny / 4, 2)) {
+        cylinder[i][j] = 1;
+      } else {
+        cylinder[i][j] = 0;
+    }
+  }
 
   // Simulation
   for (int i = 0; i < Nt; i++) {
@@ -112,11 +182,119 @@ int main() {
 
     // Drift IDK about this one
     for (int j = 0; j < NL; j++) {
-      //roll_array(j, cxs[j], cys[j], F);
+      roll_array(j, cxs[j], cys[j], F);
     }
+
+    // bndryF = F[cylinder,:]
+    // its 2d of size 1941x9 but no idea how this is calculated ??????
+    double ***bndryF = malloc_3d(Nx, Ny, NL);
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        if (cylinder[j][k] == 1) {
+          for (int l = 0; l < NL; l++) {
+            bndryF[j][k][l] = F[j][k][l];
+          }
+        }
+      }
+    }
+
+    // translate bndryF = bndryF[:,[0,5,6,7,8,1,2,3,4]]
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        // TODO bnryF[:,[0,5,6,7,8,1,2,3,4]]
+      }
+    }
+
+    // np.sum(F,2) sum over 2nd axis
+    double **rho = malloc_2d_double(Nx, Ny);
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        for (int l = 0; l < NL; l++) {
+          rho[j][k] += F[j][k][l];
+        }
+      }
+    }
+
+    // ux = np.sum(F * cxs, 2) / rho
+    double **ux = malloc_2d_double(Nx, Ny);
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        for (int l = 0; l < NL; l++) {
+          ux[j][k] += F[j][k][l] * cxs[l];
+        }
+        ux[j][k] /= rho[j][k];
+      }
+    }
+
+    // uy = np.sum(F * cys, 2) / rho
+    double **uy = malloc_2d_double(Nx, Ny);
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        for (int l = 0; l < NL; l++) {
+          uy[j][k] += F[j][k][l] * cys[l];
+        }
+        uy[j][k] /= rho[j][k];
+      }
+    }
+
+    double ***Feq = malloc_3d(Nx, Ny, NL);
+    // set to zero
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        for (int l = 0; l < NL; l++) {
+          Feq[j][k][l] = 0;
+        }
+      }
+    }
+
+    //for i, cx, cy, w in zip(idxs, cxs, cys, weights):
+    //        Feq[:,:,i] = rho * w * ( 1 + 3*(cx*ux+cy*uy)  + 9*(cx*ux+cy*uy)**2/2 - 3*(ux**2+uy**2)/2 )
+    for(int k = 0; k < NL; k++) {
+      for (int j = 0; j < Nx; j++) {
+        for (int l = 0; l < Ny; l++) {
+          Feq[j][l][k] = rho[j][l] * w[k] * (1 + 3 * (cxs[k] * ux[j][l] + cys[k] * uy[j][l]) + 9 * pow(cxs[k] * ux[j][l] + cys[k] * uy[j][l], 2) / 2 - 3 * (pow(ux[j][l], 2) + pow(uy[j][l], 2)) / 2);
+        }
+      }
+    }
+
+    // F += -(1.0/tau) * (F - Feq)
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        for (int l = 0; l < NL; l++) {
+          F[j][k][l] += -(1.0 / tau) * (F[j][k][l] - Feq[j][k][l]);
+        }
+      }
+    }
+
+    // Apply boundary 
+    //F[cylinder,:] = bndryF
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        if (cylinder[j][k] == 1) {
+          for (int l = 0; l < NL; l++) {
+            F[j][k][l] = bndryF[j][k][l];
+          }
+        }
+      }
+    }
+
+    //vorticity = (np.roll(ux, -1, axis=0) - np.roll(ux, 1, axis=0)) - (np.roll(uy, -1, axis=1) - np.roll(uy, 1, axis=1))
+    //vorticity[cylinder] = np.nan
+    //vorticity = np.ma.array(vorticity, mask=cylinder)
+    double **vorticity = malloc_2d_double(Nx, Ny);
+    for (int j = 0; j < Nx; j++) {
+      for (int k = 0; k < Ny; k++) {
+        vorticity[j][k] = 1; // TODO
+      }
+    }
+
+
+    
+
   }
+
 
   return 0;
 }
 
-// to run and compile run clear && gcc -o 2D/2D 2D/main.cpp -lm && 2D/2D
+// to run and compile run: clear && gcc -o 2D/2D 2D/main.cpp -lm && 2D/2D
---
+++ 2D/reference-python/latticeboltzmann_prints.py
@@ -0,0 +1,137 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from inspect import currentframe
+
+"""
+Create Your Own Lattice Boltzmann Simulation (With Python)
+Philip Mocz (2020) Princeton Univeristy, @PMocz
+
+Simulate flow past cylinder
+for an isothermal fluid
+
+"""
+
+
+
+def main():
+    """ Lattice Boltzmann Simulation """
+    
+    # Simulation parameters
+    Nx                     = 400    # resolution x-dir
+    Ny                     = 100    # resolution y-dir
+    rho0                   = 100    # average density
+    tau                    = 0.6    # collision timescale
+    Nt                     = 1   # number of timesteps
+    plotRealTime = True # switch on for plotting as the simulation goes along
+    
+    # Lattice speeds / weights
+    NL = 9
+    idxs = np.arange(NL)
+    cxs = np.array([0, 0, 1, 1, 1, 0,-1,-1,-1])
+    cys = np.array([0, 1, 1, 0,-1,-1,-1, 0, 1])
+    weights = np.array([4/9,1/9,1/36,1/9,1/36,1/9,1/36,1/9,1/36]) # sums to 1
+    
+    # Initial Conditions
+    F = np.ones((Ny,Nx,NL)) #* rho0 / 
+    
+    print("Line: " + str(currentframe().f_lineno) + " size of F: ", str(F.shape))
+    
+    np.random.seed(42)
+    F += 0.01*np.random.randn(Ny,Nx,NL)
+    X, Y = np.meshgrid(range(Nx), range(Ny))
+    
+    print("Line: " + str(currentframe().f_lineno) + " size of X: ", str(X.shape))
+    print("Line: " + str(currentframe().f_lineno) + " size of Y: ", str(Y.shape))
+    
+    F[:,:,3] += 2 * (1+0.2*np.cos(2*np.pi*X/Nx*4))
+    rho = np.sum(F,2)
+    
+    print("Line: " + str(currentframe().f_lineno) + " size of rho: ", str(rho.shape))
+
+    for i in idxs:
+        F[:,:,i] *= rho0 / rho
+    
+    # Cylinder boundary
+    X, Y = np.meshgrid(range(Nx), range(Ny))
+    
+    print("Line: " + str(currentframe().f_lineno) + " size of cylinder X: ", str(X.shape))
+    print("Line: " + str(currentframe().f_lineno) + " size of cylinder Y: ", str(Y.shape))
+    
+    cylinder = (X - Nx/4)**2 + (Y - Ny/2)**2 < (Ny/4)**2
+    
+    print("Line: " + str(currentframe().f_lineno) + " size of cylinder: ", str(cylinder.shape))
+
+    
+    # Prep figure
+    fig = plt.figure(figsize=(4,2), dpi=80)
+    
+    # Simulation Main Loop
+    for it in range(Nt):
+        print(it)
+        
+        # Drift
+        for i, cx, cy in zip(idxs, cxs, cys):
+            F[:,:,i] = np.roll(F[:,:,i], cx, axis=1)
+            F[:,:,i] = np.roll(F[:,:,i], cy, axis=0)
+        
+        
+        # Set reflective boundaries
+        bndryF = F[cylinder,:]
+        print("Line: " + str(currentframe().f_lineno) + " size of bndryF: ", str(bndryF.shape))
+        bndryF = bndryF[:,[0,5,6,7,8,1,2,3,4]]
+        print("Line: " + str(currentframe().f_lineno) + " size of bndryF: ", str(bndryF.shape))
+    
+        
+        # Calculate fluid variables
+        rho = np.sum(F,2)
+        print("Line: " + str(currentframe().f_lineno) + " size of rho: ", str(rho.shape))
+        ux  = np.sum(F*cxs,2) / rho
+        print("Line: " + str(currentframe().f_lineno) + " size of ux: ", str(ux.shape))
+        uy  = np.sum(F*cys,2) / rho
+        print("Line: " + str(currentframe().f_lineno) + " size of uy: ", str(uy.shape))
+        
+        
+        # Apply Collision
+        Feq = np.zeros(F.shape)
+        print("Line: " + str(currentframe().f_lineno) + " size of Feq: ", str(Feq.shape))
+        for i, cx, cy, w in zip(idxs, cxs, cys, weights):
+            Feq[:,:,i] = rho * w * ( 1 + 3*(cx*ux+cy*uy)  + 9*(cx*ux+cy*uy)**2/2 - 3*(ux**2+uy**2)/2 )
+        
+        F += -(1.0/tau) * (F - Feq)
+        
+        # Apply boundary 
+        F[cylinder,:] = bndryF
+        
+        
+        # plot in real time - color 1/2 particles blue, other half red
+        if (plotRealTime and (it % 10) == 0) or (it == Nt-1):
+            plt.cla()
+            ux[cylinder] = 0
+            uy[cylinder] = 0
+            vorticity = (np.roll(ux, -1, axis=0) - np.roll(ux, 1, axis=0)) - (np.roll(uy, -1, axis=1) - np.roll(uy, 1, axis=1))
+            print("Line: " + str(currentframe().f_lineno) + " size of vorticity: ", str(vorticity.shape))
+            vorticity[cylinder] = np.nan
+            vorticity = np.ma.array(vorticity, mask=cylinder)
+            plt.imshow(vorticity, cmap='bwr')
+            plt.imshow(~cylinder, cmap='gray', alpha=0.3)
+            plt.clim(-.1, .1)
+            ax = plt.gca()
+            ax.invert_yaxis()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)    
+            ax.set_aspect('equal')    
+            plt.pause(0.001)
+            
+    
+    # Save figure
+    plt.savefig('latticeboltzmann.png',dpi=240)
+    plt.show()
+        
+    return 0
+
+
+
+if __name__== "__main__":
+  main()
+
