7568249384d80e39d9c64e2b9cf459e1febf8a0d
Karlo Piskor <kpiskor@student.ethz.ch>
2024-04-26 17:58:12 +0200

    added writing and reading npy files and started plotter file to verify result

--- 2D/main.cpp
+++ 2D/main.cpp
@@ -5,23 +5,26 @@
 
 #include <cmath>
 #include <exception>
+#include <stdexcept>
+#include "include/npy.hpp"
+#include <vector>
+#include <string>
 
 #define PRINT 1
 
-#define debug_printf(fmt, ...) \
-  do {                         \
-    if (PRINT) {               \
+#define debug_printf(fmt, ...)           \
+  do {                                   \
+    if (PRINT) {                         \
       fprintf(stdout, fmt, __VA_ARGS__); \
-    }                          \
+    }                                    \
   } while (0)
 
-#define debug_print(fmt) \
-  do {                   \
-    if (PRINT) {         \
+#define debug_print(fmt)    \
+  do {                      \
+    if (PRINT) {            \
       fprintf(stdout, fmt); \
-    }                    \
+    }                       \
   } while (0)
-
 
 using namespace std;
 
@@ -29,7 +32,7 @@ const int Nx = 400;    // resolution in x
 const int Ny = 100;    // resolution in y
 const int rho0 = 100;  // average density
 const int tau = 0.6;   // collision timescale
-const int Nt = 4000;   // number of timesteps
+const int Nt = 100;   // number of timesteps
 
 // Lattice speeds / weights
 const int NL = 9;
@@ -68,52 +71,145 @@ void roll_array(int i, int cx, int cy, double ***F) {
   }
 }
 
-double*** malloc_3d(int x, int y, int z) {
-  double ***array = (double***)malloc(x * sizeof(double**));
+
+// TODO if fancy make generic functions for double, float, int and so on
+
+double ***malloc_3d(int x, int y, int z) {
+  double ***array = (double ***)malloc(x * sizeof(double **));
   for (int i = 0; i < x; ++i) {
-    array[i] = (double**)malloc(y * sizeof(double*));
+    array[i] = (double **)malloc(y * sizeof(double *));
     for (int j = 0; j < y; ++j) {
-      array[i][j] = (double*)malloc(z * sizeof(double));
+      array[i][j] = (double *)malloc(z * sizeof(double));
     }
   }
   return array;
 }
 
-
-int** malloc_2d(int x, int y) {
-  int **array = (int**)malloc(x * sizeof(int*));
+int **malloc_2d(int x, int y) {
+  int **array = (int **)malloc(x * sizeof(int *));
   for (int i = 0; i < x; ++i) {
-    array[i] = (int*)malloc(y * sizeof(int));
+    array[i] = (int *)malloc(y * sizeof(int));
   }
   return array;
 }
 
-
-double** malloc_2d_double(int x, int y) {
-  double **array = (double**)malloc(x * sizeof(double*));
+double **malloc_2d_double(int x, int y) {
+  double **array = (double **)malloc(x * sizeof(double *));
   for (int i = 0; i < x; ++i) {
-    array[i] = (double*)malloc(y * sizeof(double));
+    array[i] = (double *)malloc(y * sizeof(double));
   }
   return array;
 }
 
+void save_npy_3d_double(double ***array, int x, int y, int z, string filename) {
+  
+  npy::npy_data_ptr<double> d;
+  d.data_ptr = &array[0][0][0];
+  d.shape = {(unsigned long)x, (unsigned long)y, (unsigned long)z};
+  d.fortran_order = false; // optional
+
+  const std::string path{filename};
+  npy::write_npy(path, d);
+}
+
+void save_npy_2d_double(double **array, int x, int y, string filename) {
+  npy::npy_data_ptr<double> d;
+  d.data_ptr = &array[0][0];
+  d.shape = {(unsigned long)x, (unsigned long)y};
+  d.fortran_order = false; // optional
+
+  const std::string path{filename};
+  npy::write_npy(path, d);
+}
+
+void save_npy_2d_int(int **array, int x, int y, string filename) {
+  debug_printf("Saving %s\n", filename.c_str());
+
+  debug_printf("Pointer: %p\n", &array[0][0]);
+
+  // print first 10 values
+  for (int i = 0; i < 10; i++) {
+    debug_printf("%d ", array[0][i]);
+  }
+
+  debug_print("\n");
+
+  npy::npy_data_ptr<int> d;
+  d.data_ptr = &array[0][0];
+  d.shape = {(unsigned long)x, (unsigned long)y};
+  d.fortran_order = false; // optional
+
+  debug_printf("Creating %s\n", filename.c_str());
+
+  const std::string path{filename};
+  npy::write_npy(path, d);
+
+  debug_printf("Saved %s\n", filename.c_str());
+}
+
+string prepare_output() {
+  // check if output folder exists in the current folder
+  // if not create it
+  if (system("ls | grep output") != 0) {
+    debug_print("Creating main output folder\n");
+    system("mkdir output");
+
+    // check if output folder was created
+    if (system("ls | grep output") != 0) {
+      debug_print("Could not create output folder\n");
+      //throw runtime_error("Could not create output folder");
+    }
+  }
+
+  // create new folder of format YYYY_MM_DD_HH_MM_SS
+  time_t now = time(0);
+  tm *ltm = localtime(&now);
+
+  char folder_name[100];
+  sprintf(folder_name, "output/%d_%d_%d_%d_%d_%d", 1900 + ltm->tm_year,
+          1 + ltm->tm_mon, ltm->tm_mday, ltm->tm_hour, ltm->tm_min,
+          ltm->tm_sec);
+
+  char command[100];
+  sprintf(command, "mkdir output/%d_%d_%d_%d_%d_%d", 1900 + ltm->tm_year,
+          1 + ltm->tm_mon, ltm->tm_mday, ltm->tm_hour, ltm->tm_min,
+          ltm->tm_sec);
+
+  // create folder in output folder
+  //system("cd output");
+
+  // "mkdir" + folder_name
+
+  system((const char *)command);
+  //system("cd ..");
+
+  if (system(folder_name) != 0) {
+    debug_printf("Could not create output folder with name %s\n", folder_name);
+    /*throw runtime_error("Could not create output folder with name " +
+                        string(folder_name));*/
+  }
+
+  return string(folder_name);
+}
 
 // TODO CHECK TYPES IF ALL CORRECT
 // TODO arrays need to be initialized likely with 0 values
 int main() {
+  string folder_name = prepare_output(); // TODO delete empty folders
+
+  debug_printf("Output folder: %s\n", folder_name.c_str());
 
   // Lattice Boltzmann Simulation in 2D
   debug_print("Starting\n");
 
-  
   // Initial conditions
   // distribution function
- 
+
   // TODO creating array directly segfaults because of size
   double ***F = malloc_3d(Nx, Ny, NL);
   debug_print("Initializing\n");
 
-  //srand(42);  // some seed
+  // srand(42);  // some seed
 
   debug_print("Seed set\n");
 
@@ -126,10 +222,11 @@ int main() {
     }
   }
 
+  save_npy_3d_double(F, Nx, Ny, NL, folder_name + "/F.npy");
+
   // TODO maybe also malloc at some point
   int **x_coords = malloc_2d(Nx, Ny);
   int **y_coords = malloc_2d(Nx, Ny);
-  
 
   meshgrid(x_coords, y_coords);
 
@@ -169,12 +266,18 @@ int main() {
   // cylinder = (X - Nx/4)**2 + (Y - Ny/2)**2 < (Ny/4)**2
   for (int i = 0; i < Nx; i++) {
     for (int j = 0; j < Ny; j++) {
-      if ((pow(x_coords[i][j] - Nx / 4, 2) + pow(y_coords[i][j] - Ny / 2, 2)) < pow(Ny / 4, 2)) {
+      if ((pow(x_coords[i][j] - Nx / 4, 2) + pow(y_coords[i][j] - Ny / 2, 2)) <
+          pow(Ny / 4, 2)) {
         cylinder[i][j] = 1;
       } else {
         cylinder[i][j] = 0;
+      }
     }
   }
+
+  save_npy_2d_int(cylinder, Nx, Ny, folder_name + "/cylinder.npy");
+
+  debug_print("Starting simulation\n");
 
   // Simulation
   for (int i = 0; i < Nt; i++) {
@@ -247,12 +350,19 @@ int main() {
       }
     }
 
-    //for i, cx, cy, w in zip(idxs, cxs, cys, weights):
-    //        Feq[:,:,i] = rho * w * ( 1 + 3*(cx*ux+cy*uy)  + 9*(cx*ux+cy*uy)**2/2 - 3*(ux**2+uy**2)/2 )
-    for(int k = 0; k < NL; k++) {
+    debug_printf("Feq shape: %d %d %d\n", Nx, Ny, NL);
+
+    // for i, cx, cy, w in zip(idxs, cxs, cys, weights):
+    //         Feq[:,:,i] = rho * w * ( 1 + 3*(cx*ux+cy*uy)  +
+    //         9*(cx*ux+cy*uy)**2/2 - 3*(ux**2+uy**2)/2 )
+    for (int k = 0; k < NL; k++) {
       for (int j = 0; j < Nx; j++) {
         for (int l = 0; l < Ny; l++) {
-          Feq[j][l][k] = rho[j][l] * w[k] * (1 + 3 * (cxs[k] * ux[j][l] + cys[k] * uy[j][l]) + 9 * pow(cxs[k] * ux[j][l] + cys[k] * uy[j][l], 2) / 2 - 3 * (pow(ux[j][l], 2) + pow(uy[j][l], 2)) / 2);
+          Feq[j][l][k] =
+              rho[j][l] * w[k] *
+              (1 + 3 * (cxs[k] * ux[j][l] + cys[k] * uy[j][l]) +
+               9 * pow(cxs[k] * ux[j][l] + cys[k] * uy[j][l], 2) / 2 -
+               3 * (pow(ux[j][l], 2) + pow(uy[j][l], 2)) / 2);
         }
       }
     }
@@ -266,8 +376,8 @@ int main() {
       }
     }
 
-    // Apply boundary 
-    //F[cylinder,:] = bndryF
+    // Apply boundary
+    // F[cylinder,:] = bndryF
     for (int j = 0; j < Nx; j++) {
       for (int k = 0; k < Ny; k++) {
         if (cylinder[j][k] == 1) {
@@ -278,23 +388,22 @@ int main() {
       }
     }
 
-    //vorticity = (np.roll(ux, -1, axis=0) - np.roll(ux, 1, axis=0)) - (np.roll(uy, -1, axis=1) - np.roll(uy, 1, axis=1))
-    //vorticity[cylinder] = np.nan
-    //vorticity = np.ma.array(vorticity, mask=cylinder)
+    // vorticity = (np.roll(ux, -1, axis=0) - np.roll(ux, 1, axis=0)) -
+    // (np.roll(uy, -1, axis=1) - np.roll(uy, 1, axis=1)) vorticity[cylinder]
+    // = np.nan vorticity = np.ma.array(vorticity, mask=cylinder)
     double **vorticity = malloc_2d_double(Nx, Ny);
     for (int j = 0; j < Nx; j++) {
       for (int k = 0; k < Ny; k++) {
-        vorticity[j][k] = 1; // TODO
+        //vorticity[j][k] = (ux[(j + 1 + Nx) % Nx][k] - ux[(j - 1 + Nx) % Nx][k]) -
+        //                  (uy[j][(k + 1 + Ny) % Ny] - uy[j][(k - 1 + Ny) % Ny]);
       }
     }
 
-
-    
-
+    // Save vorticity
+    save_npy_2d_double(vorticity, Nx, Ny, folder_name + "/step_" + to_string(i) + "_vorticity.npy");
   }
-
 
   return 0;
 }
 
-// to run and compile run: clear && gcc -o 2D/2D 2D/main.cpp -lm && 2D/2D
+// to run and compile run: clear && g++ -o 2D/2D 2D/main.cpp -lm && 2D/2D
---
+++ 2D/plot.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# get all folders in output folders
+import os
+
+# get all folders in output folders
+output_folders = [x[0] for x in os.walk("output/")][1:]
+
+# if the output folder is empty, remove it
+for folder in output_folders:
+    if len(os.listdir(folder)) == 0:
+        output_folders.remove(folder)
+        # delete the folder
+        os.rmdir(folder)
+    
+    
+# order folders descending
+output_folders.sort(reverse=True)
+    
+# present list of folders to select as a list [0] name of folder
+# TODO show may last x ones
+print("Select a folder to plot:")
+for i, folder in enumerate(output_folders):
+    print(f"[{i}] {folder}")
+    
+# select a folder
+folder = output_folders[int(input("Enter the number of the folder to plot: "))]
+print(f"Selected folder: {folder}")
+
+# in that folder find npy files and display its shape and first few elements
+files = os.listdir(folder)
+npy_files = [file for file in files if file.endswith(".npy")]
+
+# take first file
+file = npy_files[0]
+print(f"Selected file: {file}")
+
+# load the file
+data = np.load(f"{folder}/{file}")
+
+print(f"Shape of data: {data.shape}")
+print(f"Data: {data[:5]}")
+
+
+
+# get all .npy that start with step_
+npy_files = [file for file in files if file.startswith("step_")]
+
+# sort the files
+npy_files.sort()
+
+
+cylinder = np.load("cylinder.npy") # todo store cylinder numpy array in the output folder
+
+
+# plot the files
+count = 0
+
+for file in npy_files:
+    
+    print(f"Plotting file: {file}")
+    
+    # load the file
+    vorticity = np.load(f"{folder}/{file}")
+    
+    plt.cla()
+
+    vorticity = np.ma.array(vorticity, mask=cylinder)
+    plt.imshow(vorticity, cmap='bwr')
+    plt.imshow(~cylinder, cmap='gray', alpha=0.3)
+    plt.clim(-.1, .1)
+    ax = plt.gca()
+    ax.invert_yaxis()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)    
+    ax.set_aspect('equal')    
+    plt.pause(0.001)
