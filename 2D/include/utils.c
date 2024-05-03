389479e8467f9a73233a3ceeca20c4ecd1742738
Albert Cerfeda <cerfeda.albert@crfda.com>
2024-05-03 19:00:32 +0000

    Added clang-format file

--- 2D/include/utils.c
+++ 2D/include/utils.c
@@ -1,26 +1,27 @@
-#include <string>
-#include <vector>
-#include <sys/stat.h>
-#include "./npy.hpp"
 #include "./utils.h"
+#include "./npy.hpp"
+#include <string>
+#include <sys/stat.h>
+#include <vector>
 
 #define debug_printf(fmt, ...) fprintf(stdout, fmt, __VA_ARGS__)
 #define debug_print(fmt) fprintf(stdout, fmt)
 
-void save_npy_3d_double(double *array, int x, int y, int z, std::string filename) {
-  // convert array to vector  
-  // this is needed because npy::write_npy expects a vector and cant deal with pointers to arrays somehow
+void save_npy_3d_double(double *array, int x, int y, int z,
+                        std::string filename) {
+  // convert array to vector
+  // this is needed because npy::write_npy expects a vector and cant deal with
+  // pointers to arrays somehow
 
   std::vector<double> vec;
 
   for (int i = 0; i < x; i++) {
     for (int j = 0; j < y; j++) {
       for (int k = 0; k < z; k++) {
-        vec.push_back(array[i*(y*z) +j*(z) +k]);
+        vec.push_back(array[i * (y * z) + j * (z) + k]);
       }
     }
   }
-
 
   npy::npy_data<double> d;
   d.data = vec;
@@ -31,19 +32,18 @@ void save_npy_3d_double(double *array, int x, int y, int z, std::string filename
   npy::write_npy(path, d);
 }
 
-
 void save_npy_2d_double(double *array, int x, int y, std::string filename) {
-  // convert array to vector  
-  // this is needed because npy::write_npy expects a vector and cant deal with pointers to arrays somehow
+  // convert array to vector
+  // this is needed because npy::write_npy expects a vector and cant deal with
+  // pointers to arrays somehow
 
   std::vector<double> vec;
 
   for (int i = 0; i < x; i++) {
     for (int j = 0; j < y; j++) {
-      vec.push_back(array[i*y +j]);
+      vec.push_back(array[i * y + j]);
     }
   }
-
 
   npy::npy_data<double> d;
   d.data = vec;
@@ -54,16 +54,16 @@ void save_npy_2d_double(double *array, int x, int y, std::string filename) {
   npy::write_npy(path, d);
 }
 
-
 void save_npy_2d_int(int *array, int x, int y, std::string filename) {
   // convert array to vector
-  // this is needed because npy::write_npy expects a vector and cant deal with pointers to arrays somehow
-  // NOTE THIS FUNCTION SHOULD LIKELY NOT BE USED AS INT STORING SOMEHOW DOESNT WORK AND SAVES INCORRECTLY THE DATA
+  // this is needed because npy::write_npy expects a vector and cant deal with
+  // pointers to arrays somehow NOTE THIS FUNCTION SHOULD LIKELY NOT BE USED AS
+  // INT STORING SOMEHOW DOESNT WORK AND SAVES INCORRECTLY THE DATA
 
   std::vector<int> vec;
   for (int i = 0; i < x; i++) {
     for (int j = 0; j < y; j++) {
-      vec.push_back(array[i*y +j]);
+      vec.push_back(array[i * y + j]);
     }
   }
 
@@ -76,10 +76,9 @@ void save_npy_2d_int(int *array, int x, int y, std::string filename) {
   npy::write_npy(path, d);
 }
 
-
-
-/// @brief This function prepares the output main output folder and creates a new folder with the current date and time
-/// @return 
+/// @brief This function prepares the output main output folder and creates a
+/// new folder with the current date and time
+/// @return
 std::string make_output_folder() {
   // create new folder of format YYYY_MM_DD_HH_MM_SS
   time_t now = time(0);
@@ -87,9 +86,9 @@ std::string make_output_folder() {
 
   // its this ugly because i cant be bothered to do const char shenanigans
   char folder_name[100];
-  sprintf(folder_name, "output/%02d_%02d_%02d_%02d_%02d_%02d", 1900 + ltm->tm_year,
-          1 + ltm->tm_mon, ltm->tm_mday, ltm->tm_hour, ltm->tm_min,
-          ltm->tm_sec);
+  sprintf(folder_name, "output/%02d_%02d_%02d_%02d_%02d_%02d",
+          1900 + ltm->tm_year, 1 + ltm->tm_mon, ltm->tm_mday, ltm->tm_hour,
+          ltm->tm_min, ltm->tm_sec);
 
   if (mkdir(folder_name, 0755) == -1) {
     debug_printf("Could not create output folder with name %s\n", folder_name);
@@ -100,7 +99,7 @@ std::string make_output_folder() {
   return std::string(folder_name);
 }
 
-void make_latest_output(std::string folder){
+void make_latest_output(std::string folder) {
   // check if output/latest folder exists in the current folder
   // if it does exist delete it
   if (system("ls | grep output/00_latest") > 0) {
--- 2D/include/utils.h
+++ 2D/include/utils.h
@@ -1,7 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void save_npy_3d_double(double *array, int x, int y, int z, std::string filename);
+void save_npy_3d_double(double *array, int x, int y, int z,
+                        std::string filename);
 void save_npy_2d_double(double *array, int x, int y, std::string filename);
 void save_npy_2d_int(int *array, int x, int y, std::string filename);
 
