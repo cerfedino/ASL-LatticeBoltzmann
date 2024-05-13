2670e95725e5bd59ced11aa7df24c2f506ff4a12
DominicWuest <dominic.wuest@hotmail.com>
2024-05-13 19:52:05 +0200

    Fix roofline plot positions and values

--- 2D/benchmark.py
+++ 2D/benchmark.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import re
+import math
 import numpy as np
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
@@ -41,7 +42,7 @@ PEAK_simd = PEAK_SCALAR * (SIMD_LEN_BITS/64)
 
 
 def make_roofline_plot(PEAK_SCALAR, MEM_BW, SIMD_LEN_BITS):
-    XMIN = 0.03125
+    XMIN = 1E-5
     XMAX = 2**4
     
     PLT_FACECOLOR="#E4E4E4"
@@ -51,44 +52,45 @@ def make_roofline_plot(PEAK_SCALAR, MEM_BW, SIMD_LEN_BITS):
     fig = plt.figure(figsize=(20, 12), facecolor="white")
     plt.gca().set_facecolor(PLT_FACECOLOR)
     
+    plt.xscale("log", base=2)
+    plt.yscale("log", base=2)
     
     #  Memory bound
     x = np.linspace(XMIN, XMAX, 100)
-    y_mem_scalar = [(MEM_BW * i) for i in x ]
-    plt.plot(x, y_mem_scalar, color=PLT_BOUND_COLOR, linestyle='--', linewidth=1.4)
+    memory_line, = plt.plot((XMIN, XMAX), (MEM_BW*XMIN, MEM_BW*XMAX), color=PLT_BOUND_COLOR, linestyle='--', linewidth=1.4)
     
     ### No SIMD bounds
     # Compute bound
     y_comp_scalar = [PEAK_SCALAR for i in x]
     plt.plot(x, y_comp_scalar, color=PLT_BOUND_COLOR, linestyle='--', linewidth=1.4)
-    plt.text(0.04,y_comp_scalar[0], f"Peak $\pi$ scalar ({y_comp_scalar[0]} iops/cycle)", fontsize=12, va='bottom', ha='left', color=PLT_BOUND_COLOR) 
+    plt.text(0.04,y_comp_scalar[0], f"Peak $\pi$ scalar ({y_comp_scalar[0]} iops/cycle)", fontweight="bold", fontsize=12, va='bottom', ha='left', color=PLT_BOUND_COLOR) 
 
-    plt.text(0.035, 0.6, f"Read $\\beta$ ({MEM_BW} Bytes/Cycle)", fontsize=12, va='bottom', ha='left', color=PLT_BOUND_COLOR, rotation=33)
-
-    # plt.plot(x, [min(y_mem_scalar[i], y_comp_scalar[i]) for i in range(len(x))], color='black', linestyle='-', linewidth=1.4)
+    ax = plt.gca()
+    memory_coords = memory_line.get_data()
+    p1 = ax.transData.transform_point(memory_coords[0])
+    p2 = ax.transData.transform_point(memory_coords[-1])
+    text_rot = 90 - np.degrees(np.arctan2(p2[1] - p1[1], p2[0] - p1[0]))
+    text = plt.text(np.min(x) * 1.5, MEM_BW * np.min(x) * 1.5 * 1.5, f"Read $\\beta$ ({MEM_BW} Bytes/Cycle)", fontweight="bold", fontsize=12, va='bottom', ha='left', color=PLT_BOUND_COLOR, rotation=text_rot)
 
     ### SIMD bounds
     # Compute bound
     y_comp_simd = [PEAK_simd for i in x]
     plt.plot(x, y_comp_simd, color=PLT_BOUND_COLOR, linestyle='--', linewidth=1.4)
-    plt.text(0.04,y_comp_simd[0], f"Peak $\pi$ SIMD ({y_comp_simd[0]} iops/cycle)", fontsize=12, va='bottom', ha='left', color=PLT_BOUND_COLOR)
+    plt.text(0.04,y_comp_simd[0], f"Peak $\pi$ SIMD ({y_comp_simd[0]} iops/cycle)", fontweight="bold", fontsize=12, va='bottom', ha='left', color=PLT_BOUND_COLOR)
 
     plt.gca().yaxis.grid(True, which='major', color='w', linestyle='-', linewidth=1.5)
     plt.xlabel("Operational Intensity I(n) [iops/byte]", fontsize=14, rotation=0, labelpad=5, ha='center')
     plt.ylabel("Performance P(n) [iops/cycle]", fontsize=14, rotation=0, ha='left', labelpad=-15, position=(1,1))
 
     # Plot bounds
-    plt.ylim(np.min(y_mem_scalar), 2**6)
     plt.xlim(np.min(x), XMAX)
-    plt.xscale("log", base=2)
-    plt.yscale("log", base=2)
     
     
     return plt
 
 def plot_roofline(plt, label, perf, intensity, simd=False):
     plt.plot(intensity, perf, 'ro', label=label)
-    plt.text(intensity, perf, f"{label}\n ({perf:.2f} iops/cycle)", fontsize=12, va='top', ha='center', color='red')
+    plt.text(intensity, perf * 0.85, f"{label}\n ({perf:.2f} iops/cycle)", fontsize=12, va='top', ha='center', color='red')
     return plt
 
 
@@ -108,10 +110,15 @@ def main():
             continue
         previous_versions.append(PREVIOUS_VERSIONS_PATH + folder)
     
-    
+    if len(previous_versions) == 0:
+        print("No previous versions found, stopping...")
+        exit(0)
     
     # Init empty roofline plot
     plt = make_roofline_plot(PEAK_SCALAR, MEM_BW, SIMD_LEN_BITS)
+
+
+    min_performance = math.inf
     
     for path in previous_versions:
         print("=========")
@@ -136,26 +143,40 @@ def main():
         # Execute executable file in subprocess and read stdout
         output = run_executable_and_get_output(f"{path}/main_optimized.o", [f"{PARAMS[Nx]}", f"{PARAMS[Ny]}", f"{PARAMS[Nt]}"])
         
-        print(output)
         runs = int(re.search(r"Run (\d+)/\d+ done\nProfiling results:", output).group(1))
-        cycles = int(re.search(r"Cycles taken: (\d+)", output).group(1))
+        cycles_rho  = int(re.search(r".*Rho\s*Calculation: .* (\d+) cycles.*", output).group(1))
+        cycles_feq  = int(re.search(r".*FEQ\s*Calculation: .* (\d+) cycles.*", output).group(1))
+        cycles_f    = int(re.search(r".*F\s*Calculation: .* (\d+) cycles.*", output).group(1))
+        cycles_vort = int(re.search(r".*Vort\s*Calculation: .* (\d+) cycles.*", output).group(1))
+        cycles = cycles_rho + cycles_feq + cycles_f + cycles_vort
+
+        print()
+        print(f"Rho  cycles: {cycles_rho }")
+        print(f"FEQ  cycles: {cycles_feq }")
+        print(f"F    cycles: {cycles_f   }")
+        print(f"Vort cycles: {cycles_vort}")
         print(f"Total Number of Cycles: {int(cycles/runs)}")
         
         WORK_eval = int(sympify(WORK).subs(PARAMS).evalf())
         DATA_eval = int(sympify(DATA_MOVEMENT).subs(PARAMS).evalf())
         INTENSITY = WORK_eval / DATA_eval
         PERFORMANCE = WORK_eval / int(cycles/runs)
+
+        min_performance = min(min_performance, PERFORMANCE)
         
-        print("")
+        print()
         print("Work: ", WORK, "= " + str(WORK_eval))
         print("Data Movement: ", DATA_MOVEMENT, "= " + str(DATA_eval))
         print("Operational Intensity: ", INTENSITY)
         print("Performance: ", PERFORMANCE)
         
-        
         plt = plot_roofline(plt, TITLE, PERFORMANCE, INTENSITY, simd=True)
-        plt.savefig(f"{OUTPUT_FOLDER}/roofline_plot.out.pdf")
-        plt.show()
+
+
+    plt.ylim(min(min_performance, PEAK_SCALAR)/2.5, 2.5 * PEAK_simd)
+
+    plt.savefig(f"{OUTPUT_FOLDER}/roofline_plot.out.pdf")
+    plt.show()
 
         
 main()
