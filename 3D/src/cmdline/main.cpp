fe85d7dd354167958ed591843cae98b3c9183448
Albert Cerfeda <cerfeda.albert@crfda.com>
2024-05-22 15:53:51 +0000

    Updated makefiles

--- 3D/src/cmdline/main.cpp
+++ 3D/src/cmdline/main.cpp
@@ -8,76 +8,66 @@
 #include <unistd.h>
 #include "LBM.hpp"
 #include "vector3.hpp"
-//RapidJSON files.
-#include "document.h"
-#include "writer.h"
-#include "stringbuffer.h"
 #include "csv.h"
-
-using namespace rapidjson;
 
 inline bool file_exists (const std::string& name) {
   struct stat buffer;
   return (stat (name.c_str(), &buffer) == 0);
 }
 
-int main(int argc, char** argv) {
-    if(!file_exists("options.json")) {
-        std::cout << "Please ensure that options.json exists." << '\n';
-        return -1;
+
+#define save_every 20
+#define c_s  0.577350269
+#define velocity_set "D3Q15"
+#define nu 0.16666666666
+#define tau 1
+#define gamma_dot 0.01
+#define boundary_conditions "lees_edwards"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    if (argc != 5) {
+        cout << "Usage: ./lbm <Nx> <Ny> <Nz> <Nt>" << '\n';
+        return 1;
     }
+
+    int Nx = std::stoi(argv[1]);
+    int Ny = std::stoi(argv[2]);
+    int Nz = std::stoi(argv[3]);
+    int Nt = std::stoi(argv[4]);
+
+    cout << "Nx: " << Nx << " Ny: " << Ny << " Nz: " << Nz << " Nt: " << Nt << endl;
+
+
     
     system("rm -rf output");
     system("mkdir output");
 
-	std::ifstream t("options.json");
-	std::string str((std::istreambuf_iterator<char>(t)),
-	                 std::istreambuf_iterator<char>());
-	rapidjson::Document d;
-	d.Parse(str.c_str());
-	Value& save_every_value = d["save_every"];
-	int save_every = save_every_value.GetInt();
-	std::cout << "Save every: " << save_every << '\n';
-	auto grid_size = d["grid_size"].GetArray();
-	int NX = grid_size[0].GetInt();
-	int NY = grid_size[1].GetInt();
-	int NZ = grid_size[2].GetInt();
-	std::cout << "Grid size: " << NX << "x" << NY << "x" << NZ << '\n';
-	Value& m_c_s = d["c_s"];
-	double c_s = m_c_s.GetDouble();
-	std::cout << "c_s (Speed of sound): " << c_s << '\n';
-	Value& m_tau = d["tau"];
-	double tau = m_tau.GetDouble();
+
 	std::cout << "Tau: " << tau << '\n';
 	if(tau < 0.5) {
 	    //Section 3.5.5.1 with delta t=1.
 	    std::cout << "Error: Tau must be greater than 0.5 for numerical stability." << '\n';
 	    return -1;
 	}
-	Value& m_velocity_set = d["velocity_set"];
-	std::string velocity_set = m_velocity_set.GetString();
-	Value& m_boundary_conditions = d["boundary_conditions"];
-	std::string boundary_conditions = m_boundary_conditions.GetString();
-	if(velocity_set != "D3Q15" && velocity_set != "D3Q27" && velocity_set != "D2Q9") {
+
+    if(velocity_set != "D3Q15" && velocity_set != "D3Q27" && velocity_set != "D2Q9") {
 		std::cout << "Error: Please specify a valid velocity set such as D3Q15,D3Q27 or D2Q9." << '\n';
 		return -1;
 	}
-	std::cout << "Velocity set: " << velocity_set << '\n';
+
 	if(boundary_conditions != "lees_edwards" && boundary_conditions != "periodic" && boundary_conditions != "couette") {
 	    std::cout << "Errors: boundary_conditions in options.json can either be: periodic, Couette (D2Q9 only) or lees_edwards (Lees-Edwards Shear, Please see research paper by Alexander Wagner)";
 	    return -1;
 	}
-	std::cout << "Boundary conditions: " << boundary_conditions << '\n';
-	    Value& m_gamma_dot = d["gamma_dot"];
-	    double gamma_dot = m_gamma_dot.GetDouble();
-	std::cout << "Shear rate (gamma_dot): " << gamma_dot << '\n';
-	if(NZ != 1 && velocity_set == "D2Q9") {
-	    std::cout << "Warning: NZ=1 for D2Q9.";
+
+	if(Nz != 1 && velocity_set == "D2Q9") {
+	    std::cout << "Warning: Nz=1 for D2Q9.";
 	    return -1;
 	}
-    Value& m_n_steps = d["n_steps"];
-    int n_steps = m_n_steps.GetInt();
-	LBM *solver = new LBM(NX,NY,NZ, velocity_set, c_s, tau, boundary_conditions, gamma_dot);
+
+	LBM *solver = new LBM(Nx,Ny,Nz, velocity_set, c_s, tau, boundary_conditions, gamma_dot);
 	for(int i = 0; i < argc; i++) {
 		if(std::string(argv[i]) == "generate_ic") {
 			solver->output_lbm_data("ic.csv", true);
@@ -85,14 +75,15 @@ int main(int argc, char** argv) {
 			return 0;
 		}
 	}
+
 	if(file_exists("ic.csv")) {
 		std::cout << "Loading initial conditions" << '\n';
 		io::CSVReader<4> in("ic.csv");
 		in.read_header(io::ignore_extra_column, "p","u_x","u_y","u_z");
 		double density,u_x,u_y,u_z;
-		for(int i = 0; i < NX; i++) {
-			for(int j = 0; j < NY; j++) {
-				for(int k = 0; k < NZ; k++) {
+		for(int i = 0; i < Nx; i++) {
+			for(int j = 0; j < Ny; j++) {
+				for(int k = 0; k < Nz; k++) {
 					in.read_row(density,u_x,u_y,u_z);
 					solver->set_density(i,j,k,density);
 					solver->set_velocity(i,j,k,u_x,u_y,u_z);
@@ -102,8 +93,9 @@ int main(int argc, char** argv) {
 		std::cout << "Loaded initial conditions" << '\n';
 	} else {
 		std::cout << "Using default of p=1 for all x,y,z and u(x,t=0)=0 for all x,y,z. (Steady state)" << '\n';
-		std::cout << "If you wish to use your own initial conditions, please run the program but with command: generate_ic as a argument which will output ic.csv in format of p,u_x,u_y,u_z, assume indexes are incrementing i,j,k for i<NX,j<NY and k<NZ" << '\n';
+		std::cout << "If you wish to use your own initial conditions, please run the program but with command: generate_ic as a argument which will output ic.csv in format of p,u_x,u_y,u_z, assume indexes are incrementing i,j,k for i<Nx,j<Ny and k<Nz" << '\n';
 	}
+
 	//Equation 3.5 with delta t = 1, LBM Principles and Practice book.
 	double viscosity = c_s * c_s * (tau - 0.5);
 	std::cout << "Kinematic shear viscosity: " << viscosity << '\n';
@@ -112,7 +104,7 @@ int main(int argc, char** argv) {
 	solver->output_lbm_data("output/0.csv");
 	solver->output_indices_file();
 	int scale = 1;
-	int runs = n_steps * scale * scale * scale;
+	int runs = Nt * scale * scale * scale;
 	for(int i = 0; i < runs; i = i + 1) {
 		solver->perform_timestep();
 		if((i+1) % save_every == 0) {
@@ -122,6 +114,10 @@ int main(int argc, char** argv) {
             //solver->output_velocity();
         }
 	}
+    
+    std::ofstream ofs("output/timestamp.txt");
+    ofs.close();
+
 	std::cout << std::endl;
 	delete solver;
 	return 0;
