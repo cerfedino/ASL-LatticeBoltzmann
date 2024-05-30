7fd36369c3f1b1d8dac4a42b806c51c6986dd224
kivanova <kivanova@student.ethz.ch>
2024-05-30 21:18:50 +0200

    fix up profile

--- 3D/previous_versions/v3/src/LBM/include/profile.cpp
+++ 3D/previous_versions/v3/src/LBM/include/profile.cpp
@@ -3,7 +3,6 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
 
 #ifndef PROFILE
 profiler *init_profiler(uint64_t flops, uint64_t bytes) { return NULL; }
@@ -15,16 +14,16 @@ profiler_stats finish_profiler(profiler *p) {
 }
 #else
 // Returns the current cycle time as accurately as possible
-uint64_t time() {
+inline uint64_t time() {
   uint64_t t;
   __asm__ volatile("cpuid;"
-                   "lfence;"
+                   "mfence;"
                    "rdtsc;"
                    "shl  $32, %%rdx;"
                    "lea  (%%rax, %%rdx),  %0;"
                    : "=r"(t)
                    :
-                   : "rax", "rdx", "rbx", "rcx");
+                   : "rax", "rdx", "rbx", "rcx", "memory");
   return t;
 }
 
@@ -32,6 +31,7 @@ profiler *init_profiler(uint64_t flops, uint64_t bytes) {
   profiler *p = (profiler *)malloc(sizeof(profiler));
   p->_cycles = 0;
   p->_runs = 0;
+  p->_min_cycles = UINT64_MAX;
   p->flops = flops;
   p->bytes = bytes;
   return p;
@@ -45,8 +45,16 @@ void start_run(profiler *p) {
 void end_run(profiler *p) {
   uint64_t end_t = time();
   uint64_t time_diff = end_t - p->_start;
+  if (time_diff < p->_min_cycles) {
+    p->_min_cycles = time_diff;
+  }
+  if (end_t < p->_start) {
+    printf("End time is before than start time, disregarding run... end_t: %llu, p->_start: %llu, time: %llu\n", end_t, p->_start, time());
+    p->_runs--;
+    return;
+  }
   if (p->_cycles > (UINT64_MAX - time_diff)) {
-    printf("\nOVERFLOW WHEN ENDING PROFILE RUN\n");
+    printf("\nOVERFLOW WHEN ENDING PROFILE RUN, time_diff: %llu, end_t: %llu, p->_start: %llu, time: %llu\n", time_diff, end_t, p->_start, time());
     exit(1);
   }
   p->_cycles += end_t - p->_start;
@@ -54,10 +62,10 @@ void end_run(profiler *p) {
 
 profiler_stats finish_profiler(profiler *p) {
   profiler_stats ps;
-  std::cout<<p->flops<<std::endl;
-  ps.cycles = p->_cycles;
+  ps.total_cycles = p->_cycles;
+  ps.cycles = p->_min_cycles;
   ps.runs = p->_runs;
-  ps.performance = (double)(p->_runs * p->flops) / p->_cycles;
+  ps.performance = (double)(p->flops) / p->_min_cycles;
   ps.arithmetic_intensity = (double)(p->flops) / p->bytes;
   return ps;
 }
