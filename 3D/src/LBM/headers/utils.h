37ed7a0d87038affe446ae2b83269759f610cf25
DominicWuest <dominic.wuest@hotmail.com>
2024-06-05 16:23:17 +0200

    Fixy the wonky

--- 3D/src/LBM/headers/utils.h
+++ 3D/src/LBM/headers/utils.h
@@ -1,3 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #define save_every 20
 #define c_s 0.577350269
 #define velocity_set "D3Q15"
@@ -9,8 +12,8 @@
 #define omtauinv (1.0 - tauinv)
 
 #define weights_29 (2.0 / 9.0)
-#define weights_19 (1.0/9.0)
-#define weights_172 (1.0/72.0)
+#define weights_19 (1.0 / 9.0)
+#define weights_172 (1.0 / 72.0)
 #define direction_size 15
 #include <string>
 
